cfcdd47c1b5dbdcf652debd7606ff31e0e6eb8ba
Bodo Tasche <bodo@wannawork.de>
2013-09-14 12:02:21 +0200

    open options page on first install

--- javascript/background.js
+++ javascript/background.js
@@ -1,7 +1,17 @@
+function install_notice() {
+    if (localStorage.getItem("install_time"))
+        return;
+
+    var now = new Date().getTime();
+    localStorage.setItem("install_time", now);
+    chrome.tabs.create({url: "html/options.html"});
+}
+install_notice();
+
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
   if (request.method == "getStringerURLs") {
     sendResponse({urls: [
-      JSON.parse(localStorage["urls"])
+      JSON.parse(localStorage.getItem("urls"))
     ]});
   } else {
     sendResponse({});
