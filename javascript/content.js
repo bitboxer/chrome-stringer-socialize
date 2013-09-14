a16d8fa2df00d1c3a58f161fe1f151455c7844d4
Bodo Tasche <bodo@wannawork.de>
2013-09-14 09:34:32 +0200

    rough draft of the basic functionality

--- javascript/content.js
+++ javascript/content.js
@@ -1,3 +1,19 @@
 if (window.location.href.match("http(s)?://stringer.bitboxer.de/.*")) {
-  console.log("yes, this is correct");
+  $(".story-body-container").each(function(idx, el) {
+    $(".story-actions-container .story-actions", el).
+      prepend("<a href='#readability' class='story-readabilty'>" + 
+              "<img src='" + chrome.extension.getURL('images/stringer16.png')  + "'/></a>");
+    $(".story-readabilty", el).click(function() {
+      var url = $(".story-body h1 a", el).attr("href");
+      $.ajax({
+        url: "http://www.readability.com/api/content/v1/parser?url=" + encodeURIComponent(url) +
+             "&token=4a5c134cf591219667bbb8c74dc825b78bf224de",
+      }).done(function(data) {
+        $(".story-body", el).empty().append(
+          "<h1><a href='" + url + "'>" + data.title + "</a></h1>" +
+          $(data.content).html());
+      });
+      return false;
+    });
+  });
 }
